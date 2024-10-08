6dc6eb381eb2c3d73ce29a92d4ba491feae29147
Drasti Gohel <codecrafts.ccs.ca@gmail.com>
2024-10-08 11:53:27 -0400

    remove rsvp link from demo

--- src/components/data/menuData.js
+++ src/components/data/menuData.js
@@ -1,7 +1,7 @@
 export const mainMenu = [
      {link:"#", content:"Home", icon:"lucide:home"},
      {link:"#about", content:"About", icon:"octicon:person-24"},
-     {link:"/rsvp", content:"RSVP"}
+     //{link:"/rsvp", content:"RSVP"}
      // {link:"#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
      // {link:"#blog", content:"Blog", icon:"mdi:blog"}
 ]
